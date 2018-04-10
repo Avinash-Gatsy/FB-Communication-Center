f2eb3dd03473d47623ba6cc2fe102479dbc891d2
Avinash <avinashg12291992@gmail.com>
2018-04-11 03:10:19 +0530

    CORS issue - handle in middleware

--- server/app.js
+++ server/app.js
@@ -7,11 +7,20 @@ import bodyParser from 'body-parser';
 const app = express();
 
 //middleware setup
-
-mongoose.connect(config.getDBConnectionString(), ()=>{
-    console.log('connected to mongoDB');
-}); //connect to DB
+// allow-cors
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.use(bodyParser.json()); //parse any json that comes.
+app.use(bodyParser.urlencoded({ extended:true }));
+
+//connect to DB
+mongoose.connect(config.getDBConnectionString(), ()=>{
+    console.log('connected to mongoDB');
+});
+
 app.use('/api', routes);
 export default app;
